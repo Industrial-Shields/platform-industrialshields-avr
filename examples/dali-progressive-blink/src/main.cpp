95a057cb27939ff94130ccfecfa3dc93b37fc022
JoanVC100 <jojovica2001@gmail.com>
2023-12-29 15:19:02 +0100

    DALI Progressive blink example

---
+++ examples/dali-progressive-blink/src/main.cpp
@@ -0,0 +1,47 @@
+/*
+   Copyright (c) 2023 Boot&Work Corp., S.L. All rights reserved
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Arduino.h>
+
+
+#include <DALI.h>
+
+DALI dali(18, 19);
+
+uint8_t value = 0;
+uint32_t lastStepTime = millis();
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void setup() {
+  Serial.begin(9600UL);
+
+  dali.begin();
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+void loop() {
+  if (millis() - lastStepTime > 5) {
+    value += 10;
+    if (value > 254) {
+      value = 0;
+    }
+
+    dali.setValue(6, value);
+
+    lastStepTime = millis();
+  }
+}
